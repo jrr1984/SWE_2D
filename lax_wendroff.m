99141148d8f87811e1d76e2f7796dd38010c0ae3
jrr <juanreto@gmail.com>
2018-12-20 23:52:54 -0300

    agregamos SX, SY al esquema de LAX-WENDROFF

--- lax_wendroff.m
+++ lax_wendroff.m
@@ -1,5 +1,5 @@
-function [u_new, v_new, h_new] = lax_wendroff(dx, dy, dt, g, u, v, h, ...
-					      u_tendency, v_tendency);
+function [u_paso_entero, v_paso_entero, h_paso_entero] = lax_wendroff(dx, dy, dt, g, u, v, h, ...
+					      SX_n_medio, SY_n_medio);
                       
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%         ESQUEMA NUMERICO DE LAX WENDROF %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
@@ -16,7 +16,7 @@ vh = v.*h;
 
 
 h_medio_x_t = 0.5.*(h(2:end,:)+h(1:end-1,:)) ...
-  -(0.5*dt/dx).*(uh(2:end,:)-uh(1:end-1,:));
+  -(0.5*dt/dx).*(uh(2:end,:)-uh(1:end-1,:)); %h(2:end-1,:) es h_i,j
 
 %h_medio_y_t = h_{i,j+1/2}^{n+1/2}
 
@@ -39,8 +39,10 @@ Vx = Uy; %de la definicion del cambio de variables
 
 
 Vy = vh.*v+0.5.*g.*h.^2;
+
 vh_medio_x_t = 0.5.*(vh(2:end,:)+vh(1:end-1,:)) ...
   -(0.5*dt/dx).*(Vx(2:end,:)-Vx(1:end-1,:));
+
 vh_medio_y_t = 0.5.*(vh(:,2:end)+vh(:,1:end-1)) ...
   -(0.5*dt/dy).*(Vy(:,2:end)-Vy(:,1:end-1));
 
@@ -51,24 +53,28 @@ vh_medio_y_t = 0.5.*(vh(:,2:end)+vh(:,1:end-1)) ...
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-h_new = h(2:end-1,2:end-1) ...
+h_paso_entero = h(2:end-1,2:end-1) ...
   - (dt/dx).*(uh_medio_x_t(2:end,2:end-1)-uh_medio_x_t(1:end-1,2:end-1)) ...
   - (dt/dy).*(vh_medio_y_t(2:end-1,2:end)-vh_medio_y_t(2:end-1,1:end-1));
 
 Ux_medio_x_t = uh_medio_x_t.*uh_medio_x_t./h_medio_x_t + 0.5.*g.*h_medio_x_t.^2;
+
 Uy_medio_y_t = uh_medio_y_t.*vh_medio_y_t./h_medio_y_t;
-uh_new = uh(2:end-1,2:end-1) ...
+
+uh_paso_entero = uh(2:end-1,2:end-1) ...
   - (dt/dx).*(Ux_medio_x_t(2:end,2:end-1)-Ux_medio_x_t(1:end-1,2:end-1)) ...
   - (dt/dy).*(Uy_medio_y_t(2:end-1,2:end)-Uy_medio_y_t(2:end-1,1:end-1)) ...
-  + dt.*u_tendency.*0.5.*(h(2:end-1,2:end-1)+h_new);
+  + dt.*SX_n_medio.*0.5.*(h(2:end-1,2:end-1)+h_paso_entero);
 
 Vx_medio_x_t = uh_medio_x_t.*vh_medio_x_t./h_medio_x_t;
+
 Vy_medio_y_t = vh_medio_y_t.*vh_medio_y_t./h_medio_y_t + 0.5.*g.*h_medio_y_t.^2;
-s
-vh_new = vh(2:end-1,2:end-1) ...
+
+vh_paso_entero = vh(2:end-1,2:end-1) ...
   - (dt/dx).*(Vx_medio_x_t(2:end,2:end-1)-Vx_medio_x_t(1:end-1,2:end-1)) ...
   - (dt/dy).*(Vy_medio_y_t(2:end-1,2:end)-Vy_medio_y_t(2:end-1,1:end-1)) ...
-  + dt.*v_tendency.*0.5.*(h(2:end-1,2:end-1)+h_new);
-u_new = uh_new./h_new;
-v_new = vh_new./h_new;
+  + dt.*SY_n_medio.*0.5.*(h(2:end-1,2:end-1)+h_paso_entero);
+
+u_paso_entero = uh_paso_entero./h_paso_entero;
+v_paso_entero = vh_paso_entero./h_paso_entero;
 
